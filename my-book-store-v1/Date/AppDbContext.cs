c7cfa1a41b666e3142fc5fdc92eb0562aef6d471
murad <msn0033@gmail.com>
2022-09-17 14:39:10 +0300

    Add project files.

---
+++ my-book-store-v1/Date/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using my_book_store_v1.Date.Models;
+
+namespace my_book_store_v1.Date
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContext):base(dbContext)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
